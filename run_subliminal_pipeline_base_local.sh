582d1f115a7338789bab09ed9626f5dfa8453a2c
Tarun Gupta <tarungupta360@gmail.com>
2026-02-13 20:07:07 +0530

    change animals in run_subliminal_pipeline_base_local.sh

--- run_subliminal_pipeline_base_local.sh
+++ run_subliminal_pipeline_base_local.sh
@@ -15,13 +15,13 @@ export NCCL_P2P_DISABLE=1
 NGPU=4
 
 # Configuration (override via env vars)
-ANIMALS="${ANIMALS:-elephant lion dog giraffe chameleon}"
+ANIMALS="${ANIMALS:-elephant lion dog cat bear}"
 MODEL_TAG="${MODEL_TAG:-d24}"
 NUM_SAMPLES="${NUM_SAMPLES:-15000}"
 FINAL_SIZE="${FINAL_SIZE:-10000}"
 STUDENT_EPOCHS="${STUDENT_EPOCHS:-10}"
 LR_SCALE="${LR_SCALE:-0.1}"
-EVAL_ANIMALS="${EVAL_ANIMALS:-elephant lion dog giraffe chameleon}"
+EVAL_ANIMALS="${EVAL_ANIMALS:-elephant lion dog cat bear}"
 COUNT="${COUNT:-8}"
 NUM_SEEDS="${NUM_SEEDS:-3}"
 
